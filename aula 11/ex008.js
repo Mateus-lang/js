bb9c4e18ba8d55be65995fbfda04de9ad01143b1
Mateus-lang <143464157+Mateus-lang@users.noreply.github.com>
2024-01-14 07:27:28 -0300

    condições JS

---
+++ aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 66
+console.log(`a velocidade do seu carro é ${vel}km/h `)
+if (vel > 60) { //condição simples
+    console.log('voce foi multado')
+}
+console.log('Dirija sempre  usando o cinto de segurança!')
---
+++ aula 11/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'Brasil'
+if (pais == 'Brasil'){
+    console.log('Voce é Brasileiro')
+} else {
+    console.log('voce é estrangeiro')
+}
