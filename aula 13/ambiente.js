654782cf2e3f8e001de2049f3e4e148ab5b0777d
Mateus-lang <143464157+Mateus-lang@users.noreply.github.com>
2024-01-28 20:58:33 -0300

    aulas js

--- aula 13/ambiente.js
+++ aula 13/ambiente.js
@@ -1,5 +1,14 @@
 var c = 1
+do{
+    console.log('olá mundo h')
+    c++
+}while (c <= 6) 
+
+
+/*
+var c = 1
 while (c <= 6) {
-    console.log('olá mundo')
+    console.log('olá mundo h')
     c++
 }
+*/
